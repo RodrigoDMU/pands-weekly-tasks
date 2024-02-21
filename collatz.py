1b40ac362dedd5fac0f71a42eefd5aba5f002157
RodrigoRDMU <rdm.demartino@gmail.com>
2024-02-21 16:58:11 +0000

    Weekly Tasks 04

--- collatz.py
+++ collatz.py
@@ -13,11 +13,12 @@
 # Author: Rodrigo De Martino Ucedo
 
 number = int(input("Please enter a positive integer: "))
-print (number)
+print (number, end=" ") # end=" " to print number on line
 while number != 1:
     if number % 2 == 0:
         number = number // 2
-        print (number)
+        print (number, end=" ") # end=" " to print number on line
     else: 
         number = 3 * number +1
-        print (number)
+        print (number, end=" ") # end=" " to print number on line
+print (" ") # " " to print number on line without %
